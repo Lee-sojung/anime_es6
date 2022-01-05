921d8a139312b10881d12c678923bcceb9e446d8
Lee-sojung <thwjd3698@gmail.com>
2022-01-05 12:17:51 +0900

    콜백기능 추가, 디폴트 시간값 추가

--- anime.js
+++ anime.js
@@ -8,11 +8,21 @@ btn.addEventListener("click",e=>{
     animate(box,{
         prop: 'margin-left',
         value: 200,
-        duration: 800
+        duration: 800,
+        callback: ()=>{
+            animate(box,{
+                prop: 'margin-top',
+                value: 100,
+                duration:500
+            })
+        }
     })
 })
 
 function animate(selector, option){
+    //옵션에 duration 값이 없더라도 default값으로 500 지정
+    //만약에 디폴트로 설정할 값이 많으면 전개연산자로 처리
+    if(!option.duration) option.duration = 500
     const startTime = performance.now();
     requestAnimationFrame(move);
 
@@ -22,7 +32,14 @@ function animate(selector, option){
     
         if(progress < 0) progress = 0;
         if(progress > 1) progress = 1;
-        if(progress < 1) requestAnimationFrame(move);
+
+        if(progress < 1){
+            requestAnimationFrame(move);
+        //기존모션이 끝났을 때
+        }else{
+            //옵션 객체에서 callback 프로퍼티 값이 있을때에만 해당함수 호출
+            if(option.callback) option.callback();
+        } 
         console.log(timeLast);
     
         //첫번째 인수로 받은 선택자의 스타일 값은 연관배열형태로 지정하고
