249bbfdd1c7bfc4a8a5bfcde362a02093b6d3d13
Lee-sojung <thwjd3698@gmail.com>
2022-01-05 10:58:03 +0900

    반복을 돌때마다 누적시간 계산

--- anime.js
+++ anime.js
@@ -1,12 +1,25 @@
 const btn = document.querySelector("button");
 const box = document.querySelector("#box");
+const speed = 500;
 let num = 0;
+let startTime = null;
 
 btn.addEventListener("click",e=>{
     requestAnimationFrame(move);
+
+    startTime = performance.now();
+    console.log(startTime);
 })
 
-function move(){
+function move(time){
+    //move함수가 반복이 될때마다 누적시간 time에서
+    //버튼을 클릭한 시점의 시간인 startTime을 빼주면
+    //timeLast는 버튼을 클릭한 시점부터 move함수가  반복되는 누적 시간값을 나타냄
+
+    let timeLast = time-startTime;
+    console.log(timeLast);
+
+
     if(num <= 200) {
         num ++;
         requestAnimationFrame(move)
